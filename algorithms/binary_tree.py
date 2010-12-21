c840bab1d6e38270a9606b33ae3f29aa8a4ad0e3
Laurent Luce <laurentluce49@yahoo.com>
2010-12-20 17:27:54 -0800

    Node class only

--- algorithms/binary_tree.py
+++ algorithms/binary_tree.py
@@ -12,191 +12,162 @@ class Node:
     self.right = None
     self.data = data
 
-class BinaryTree:
-  """
-  Binary tree: methods to manipulate a binary tree
-  """
-  def __init__(self):
-    """
-    Binary tree constructor
-    """
-    self.root = None
-
-  def get_root(self):
-    return self.root
-
   def insert(self, data):
-    node = bt_insert(self.root, data)
-    if self.root == None:
-      self.root = node
+    """
+    Insert new node with data
 
-  def lookup(self, data):
-    return bt_lookup(self.root, data)
+    @param data node data object to insert
+    """
+    elif data < self.data:
+      if self.left == None:
+        self.left = Node(data)
+      else:
+        self.left.insert(data)
+    else:
+      if self.right == None:
+        self.right = Node(data)
+      else:
+        self.right.insert(data)
+
+  def lookup(self, data, parent=None):
+    """
+    Lookup node containing data
+
+    @param data node data object to look up
+    @param parent node's parent
+    @returns node and node's parent if found or None, None
+    """
+    if data < self.data:
+      if self.left == None:
+        return None, None
+      return self.left.lookup(data, self)
+    elif data > self.data:
+      if self.right == None:
+        return None, None
+      return self.right.lookup(data, self)
+    else:
+      return self, parent
 
   def delete(self, data):
-    return bt_delete(self.root, data)
+    """
+    Delete node containing data
 
-  def compare_trees(self, root):
-    return bt_compare_trees(self.root, root)
+    @param data node's content to delete
+    """
+    # get node containing data
+    node, parent = self.lookup(data)
+    if node != None:
+      children_count = self.children_count(node)
+      if children_count == 0:
+        # if node has no children, just remove it
+        if self.which_child(parent, node) == 'left':
+          parent.left = None
+        else:
+          parent.right = None
+      elif children_count == 1:
+        # if node has 1 child
+        # replace node by its child
+        if node.left:
+          node.data = node.left.data
+          node.left = None
+        else:
+          node.data = node.right.data
+          node.right = None
+      else:
+        # if node has 2 children
+        # find its successor
+        parent = node
+        successor = node.right
+        while successor.left:
+          parent = successor
+          successor = successor.left
+        # replace node data by its successor data
+        node.data = successor.data
+        # fix successor's parent node child
+        if parent.left == successor:
+          parent.left = successor.right
+        else:
+          parent.right = successor.right
 
+  def compare_trees(self, node):
+    """
+    Compare 2 trees
+
+    @param node tree to compare
+    @returns True if the tree passed is identical to this tree
+    """
+    if node == None:
+      return False
+    if self.data != node.data:
+      return False
+    res = True
+    if self.left == None:
+      if node.left:
+        return False
+    else:
+      res = self.left.compare_trees(node.left)
+    if self.right == None:
+      if node.right:
+        return False
+    else:
+      res = self.right.compare_trees(node.right)
+    return res
+        
   def print_tree(self):
-    bt_print_tree(self.root)
+    """
+    Print tree content inorder
+    """
+    if self.left:
+      self.left.print_tree()
+    print self.data,
+    if self.right:
+      self.right.print_tree()
 
   def tree_data(self):
-    return bt_tree_data(self.root)
+    """
+    Generator to get the tree nodes data
+    """
+    # we use a stack to traverse the tree in a non-recursive way
+    stack = []
+    node = self
+    while stack or node: 
+      if node:
+        stack.append(node)
+        node = node.left
+      else: # we are returning so we pop the node and we yield it
+        node = stack.pop()
+        yield node.data
+        node = node.right
 
-def bt_insert(root, data):
-  """
-  Insert new node with data
+  def which_child(self, parent, child):
+    """
+    Test if child is parent's left child or parent's right child
 
-  @param root tree root
-  @param data node data object
-  @returns inserted node
-  """
-  if root == None:
-    root = Node(data)
-  else:
-    if data <= root.data:
-      root.left = bt_insert(root.left, data)
+    @param parent parent node
+    @param child parent's child node
+    """
+    if parent == None:
+      return None
+    if parent.left == child:
+      return 'left'
     else:
-      root.right = bt_insert(root.right, data)
-  return root
+      return 'right'
 
-def bt_lookup(root, data, parent=None):
-  """
-  Lookup node containing data
+  def children_count(self, node):
+    """
+    Return the number of children
 
-  @param root tree root
-  @param data node data object
-  @returns node and node's parent if found or None, None
-  """
-  if root == None:
-    return None, None
-  if data < root.data:
-    return bt_lookup(root.left, data, root)
-  elif data > root.data:
-    return bt_lookup(root.right, data, root)
-  else:
-    return root, parent
-
-def bt_compare_trees(root1, root2):
-  """
-  Compare 2 trees
-
-  @param root1 tree 1 root node
-  @param root2 tree 2 root node
-  @returns True if the 2 trees are identical or False
-  """
-  if root1 == None and root2 == None:
-    return True
-  elif (root1 == None and root2 != None) or (root1 != None and root2 == None):
-    return False
-  else:
-    return (root1.data == root2.data) \
-        and bt_compare_trees(root1.left, root2.left) \
-        and bt_compare_trees(root1.right, root2.right)
-
-def bt_print_tree(root):
-  """
-  Print tree content inorder
-
-  @param root tree root node
-  """
-  if root:
-    bt_print_tree(root.left)
-    print root.data,
-    bt_print_tree(root.right)
-
-def bt_tree_data(root):
-  """
-  Generator to get the tree nodes data
-
-  @param node tree root node
-  """
-  # we use a stack to traverse the tree in a non-recursive way
-  stack = []
-  node = root
-  while stack or node: 
-    if node:
-      stack.append(node)
-      node = node.left
-    else: # we are returning so we pop the node and we yield it
-      node = stack.pop()
-      yield node.data
-      node = node.right
-
-def bt_delete(root, data):
-  """
-  Delete node containing data
-
-  @param root tree root node
-  @param data node's content to delete
-  """
-  # get node containing data
-  node, parent = bt_lookup(root, data)
-  if node != None:
-    children_count = bt_children_count(node)
-    if children_count == 0:
-      # if node has no children, just remove it
-      if bt_which_child(parent, node) == 'left':
-        parent.left = None
-      else:
-        parent.right = None
-    elif children_count == 1:
-      # if node has 1 child
-      # replace node by its child
-      if node.left:
-        node.data = node.left.data
-        node.left = None
-      else:
-        node.data = node.right.data
-        node.right = None
-    else:
-      # if node has 2 children
-      # find its successor
-      parent = node
-      successor = node.right
-      while successor.left:
-        parent = successor
-        successor = successor.left
-      node.data = successor.data
-      # if the successor has a child, replace it with its child
-      # it can only be a right child at this point
-      if successor.right:
-        successor.data = successor.right.data
-        successor.right = None
-      else:
-        parent.left = None
-
-def bt_which_child(parent, child):
-  """
-  Test if child is parent's left child or parent's right child
-
-  @param parent parent node
-  @param child parent's child node
-  """
-  if parent == None:
-    return None
-  if parent.left == child:
-    return 'left'
-  else:
-    return 'right'
-
-def bt_children_count(node):
-  """
-  Return the number of children
-
-  @param node node to get nb of children
-  @returns number of children: 0, 1, 2
-  """
-  if node == None:
-    return None
-  cnt = 0
-  if node.left:
-    cnt += 1
-  if node.right:
-    cnt += 1
-  return cnt
+    @param node node to get nb of children
+    @returns number of children: 0, 1, 2
+    """
+    if node == None:
+      return None
+    cnt = 0
+    if node.left:
+      cnt += 1
+    if node.right:
+      cnt += 1
+    return cnt
 
 
 
--- algorithms/tests/test_binary_tree.py
+++ algorithms/tests/test_binary_tree.py
@@ -1,5 +1,5 @@
 import unittest
-import binary_tree
+import algorithms.binary_tree as binary_tree
 
 class BinaryTreeTest(unittest.TestCase):
     
@@ -7,60 +7,60 @@ class BinaryTreeTest(unittest.TestCase):
 
     data = [10, 5, 15, 4, 7, 13, 17, 11, 14]
     # create 2 trees with the same content
-    bt = binary_tree.BinaryTree()
-    for i in data:
-      bt.insert(i)
+    root = binary_tree.Node(data[0])
+    for i in data[1:]:
+      root.insert(i)
 
-    bt2 = binary_tree.BinaryTree()
-    for i in data:
-      bt2.insert(i)
+    root2 = binary_tree.Node(data[0])
+    for i in data[1:]:
+      root2.insert(i)
 
     # check if both trees are identical
-    self.assertTrue(bt.compare_trees(bt2.get_root()))
+    self.assertTrue(root.compare_trees(root2))
 
     # check the content of the tree inorder
     t = []
-    for d in bt.tree_data():
+    for d in root.tree_data():
       t.append(d)
     self.assertEquals(t, [4, 5, 7, 10, 11, 13, 14, 15, 17])
 
     # test lookup
-    node, parent = bt.lookup(9)
+    node, parent = root.lookup(9)
     self.assertTrue(node == None)
     # check if returned node and parent are correct
-    node, parent = bt.lookup(11)
+    node, parent = root.lookup(11)
     self.assertTrue(node.data == 11)
     self.assertTrue(parent.data == 13)
 
     # delete a leaf node
-    bt.delete(4)
+    root.delete(4)
     # check the content of the tree inorder
     t = []
-    for d in bt.tree_data():
+    for d in root.tree_data():
       t.append(d)
     self.assertEquals(t, [5, 7, 10, 11, 13, 14, 15, 17])
 
     # delete a node with 1 child
-    bt.delete(5)
+    root.delete(5)
     # check the content of the tree inorder
     t = []
-    for d in bt.tree_data():
+    for d in root.tree_data():
       t.append(d)
     self.assertEquals(t, [7, 10, 11, 13, 14, 15, 17])
 
     # delete a node with 2 children
-    bt.delete(13)
+    root.delete(13)
     # check the content of the tree inorder
     t = []
-    for d in bt.tree_data():
+    for d in root.tree_data():
       t.append(d)
     self.assertEquals(t, [7, 10, 11, 14, 15, 17])
 
     # delete a node with 2 children
-    bt.delete(15)
+    root.delete(15)
     # check the content of the tree inorder
     t = []
-    for d in bt.tree_data():
+    for d in root.tree_data():
       t.append(d)
     self.assertEquals(t, [7, 10, 11, 14, 17])
 
