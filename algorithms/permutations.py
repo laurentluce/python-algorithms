92bb9962617937dc7933530a65ed68f7035c2255
Laurent Luce <laurentluce49@yahoo.com>
2011-03-11 15:20:01 -0800

    update description

--- algorithms/permutations.py
+++ algorithms/permutations.py
@@ -2,12 +2,22 @@ def permutations(l):
   """
   Generator for list permutations
   
-  Example: [1,2,3] = [1,2,3], [1,3,2], [2,1,3] ...
-
   @param l list to generate permutations for
   @result yield each permutation
+
+  Example:
+  l = [1,2,3]
+  a = [1]
+  permutations([2,3]) = [[2,3], [3,2]]
+  [2,3]
+  yield [1,2,3]
+  yield [2,1,3]
+  yield [2,3,1]
+  [3,2]
+  yield [1,3,2]
+  yield [3,1,2]
+  yield [3,2,1]
   """
-  print 'permutations: ',l
   if len(l) <= 1:
     yield l
   else:
@@ -15,4 +25,7 @@ def permutations(l):
     for p in permutations(l):
       for i in range(len(p)+1):
         yield p[:i] + a + p[i:]
+
+for p in permutations([1,2,3]):
+  print p
 
