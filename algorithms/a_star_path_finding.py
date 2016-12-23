13f44801ea727e82863e7a634811526bfe3af8cc
dd <doroginin@gmail.com>
2016-12-23 12:17:57 +0300

    Improve a_start algorithms: add diagonal movement mode, add mode for finding closest cell for unreachable end. Add more convenient visual way for create tests. Add new more tests.

--- algorithms/a_star_path_finding.py
+++ algorithms/a_star_path_finding.py
@@ -26,6 +26,9 @@ class Cell(object):
 
 
 class AStar(object):
+    allow_diagonal_transition = False
+    allow_closest_cell_for_unreachable_end = False
+
     def __init__(self):
         # open list
         self.opened = []
@@ -36,6 +39,8 @@ class AStar(object):
         self.cells = []
         self.grid_height = None
         self.grid_width = None
+        self.start = None
+        self.end = None
 
     def init_grid(self, width, height, walls, start, end):
         """Prepare grid cells, walls.
@@ -76,52 +81,81 @@ class AStar(object):
         """
         return self.cells[x * self.grid_height + y]
 
-    def get_adjacent_cells(self, cell):
+    def get_adjacent_cells(self, cell, r=1):
         """Returns adjacent cells to a cell.
 
         Clockwise starting from the one on the right.
 
         @param cell get adjacent cells for this cell
+        @param r selection radius
         @returns adjacent cells list.
         """
         cells = []
-        if cell.x < self.grid_width-1:
-            cells.append(self.get_cell(cell.x+1, cell.y))
+        if cell.x < self.grid_width - r:
+            for y in range(max(0, cell.y - r + 1), min(self.grid_height, cell.y + r)):
+                cells.append(self.get_cell(cell.x + r, y))
+        if (self.allow_diagonal_transition or r > 1) and cell.x < self.grid_width - r and cell.y > 0:
+            cells.append(self.get_cell(cell.x + r, cell.y - r))
         if cell.y > 0:
-            cells.append(self.get_cell(cell.x, cell.y-1))
+            for x in range(max(0, cell.x - r + 1), min(self.grid_width, cell.x + r)):
+                cells.append(self.get_cell(x, cell.y - r))
+        if (self.allow_diagonal_transition or r > 1) and cell.x > 0 and cell.y > 0:
+            cells.append(self.get_cell(cell.x - r, cell.y - r))
         if cell.x > 0:
-            cells.append(self.get_cell(cell.x-1, cell.y))
-        if cell.y < self.grid_height-1:
-            cells.append(self.get_cell(cell.x, cell.y+1))
+            for y in range(max(0, cell.y - r + 1), min(self.grid_height, cell.y + r)):
+                cells.append(self.get_cell(cell.x - r, y))
+        if (self.allow_diagonal_transition or r > 1) and cell.x > 0 and cell.y < self.grid_height - r:
+            cells.append(self.get_cell(cell.x - r, cell.y + r))
+        if cell.y < self.grid_height - r:
+            for x in range(max(0, cell.x - r + 1), min(self.grid_width, cell.x + r)):
+                cells.append(self.get_cell(x, cell.y + r))
+        if (self.allow_diagonal_transition or r > 1) and cell.x < self.grid_width - r and cell.y < self.grid_height - r:
+            cells.append(self.get_cell(cell.x + r, cell.y + r))
+
         return cells
 
     def get_path(self):
-        cell = self.end
-        path = [(cell.x, cell.y)]
-        while cell.parent is not self.start:
-            cell = cell.parent
-            path.append((cell.x, cell.y))
+        path, cell = [], None
+        if not self.end.reachable or self.end.parent is None:
+            if self.allow_closest_cell_for_unreachable_end:
+                r, max_r = 1, max(abs(self.start.x - self.end.x), abs(self.start.y - self.end.y))
+                while r < max_r:
+                    adj_cells = list(filter(lambda c: c.parent is not None, self.get_adjacent_cells(self.end, r)))
+                    if len(adj_cells) > 0:
+                        cell = min(adj_cells, key=lambda c: c.f)
+                        break
+                    r += 1
+        else:
+            cell = self.end
 
-        path.append((self.start.x, self.start.y))
-        path.reverse()
+        while cell is not None and cell.parent is not None or cell is self.start:
+            path = [(cell.x, cell.y)] + path
+            cell = cell.parent
+
         return path
 
-    def update_cell(self, adj, cell):
+    def update_cell(self, adj, cell, is_diagonal):
         """Update adjacent cell.
 
         @param adj adjacent cell to current cell
         @param cell current cell being processed
+        @param is_diagonal bool true for diagonal transition
         """
-        adj.g = cell.g + 10
+        adj.g = cell.g + (14 if is_diagonal else 10)
         adj.h = self.get_heuristic(adj)
         adj.parent = cell
         adj.f = adj.h + adj.g
 
-    def solve(self):
+    def solve(self, allow_diagonal_transition=False, allow_closest_cell_for_unreachable_end=False):
         """Solve maze, find path to ending cell.
-
-        @returns path or None if not found.
+        @returns path.
         """
+        self.allow_diagonal_transition = allow_diagonal_transition
+        self.allow_closest_cell_for_unreachable_end = allow_closest_cell_for_unreachable_end
+
+        if self.start is self.end:
+            return [(self.start.x, self.start.y)]
+
         # add starting cell to open heap queue
         heapq.heappush(self.opened, (self.start.f, self.start))
         while len(self.opened):
@@ -136,13 +170,23 @@ class AStar(object):
             adj_cells = self.get_adjacent_cells(cell)
             for adj_cell in adj_cells:
                 if adj_cell.reachable and adj_cell not in self.closed:
+                    is_diagonal = cell.x != adj_cell.x and cell.y != adj_cell.y
+                    # for diagonal transition, check availability of cells around diagonal
+                    if is_diagonal:
+                        h_cell = self.get_cell(cell.x + (adj_cell.x - cell.x), cell.y)
+                        v_cell = self.get_cell(cell.x, cell.y + (adj_cell.y - cell.y))
+                        if not h_cell.reachable or not v_cell.reachable:
+                            continue
+
                     if (adj_cell.f, adj_cell) in self.opened:
                         # if adj cell in open list, check if current path is
                         # better than the one previously found
                         # for this adj cell.
-                        if adj_cell.g > cell.g + 10:
-                            self.update_cell(adj_cell, cell)
+                        if adj_cell.g > cell.g + (14 if is_diagonal else 10):
+                            self.update_cell(adj_cell, cell, is_diagonal)
                     else:
-                        self.update_cell(adj_cell, cell)
+                        self.update_cell(adj_cell, cell, is_diagonal)
                         # add adj cell to open list
                         heapq.heappush(self.opened, (adj_cell.f, adj_cell))
+
+        return self.get_path()
--- algorithms/tests/test_a_star_path_finding.py
+++ algorithms/tests/test_a_star_path_finding.py
@@ -8,29 +8,182 @@ class Test(unittest.TestCase):
     def setUp(self):
         pass
 
+    @staticmethod
+    def draw(sketch):
+        for r in sketch:
+            print(r)
+        print()
+
+    @staticmethod
+    def to_sketch(walls, path):
+        w = 0
+        h = 0
+        for c in walls + path:
+            if c[0] + 1 > w:
+                w = c[0] + 1
+            if c[1] + 1 > h:
+                h = c[1] + 1
+        world = [[" "] * w for _ in range(h)]
+        for c in walls:
+            world[c[1]][c[0]] = '#'
+        for p in path:
+            world[p[1]][p[0]] = '.'
+        return map(lambda r: "".join(r), world)
+
+    @staticmethod
+    def from_sketch(sketch):
+        walls = []
+        path = []
+        start = None
+        end = None
+        y = 0
+        for r in sketch:
+            x = 0
+            for c in r:
+                if c == '.':
+                    path.append((x, y))
+                if c == '#':
+                    walls.append((x, y))
+                if c == '[':
+                    start = (x, y)
+                    path.append((x, y))
+                if c == ']':
+                    end = (x, y)
+                    path.append((x, y))
+                if c == '(':
+                    start = (x, y)
+                if c == ')':
+                    end = (x, y)
+                x += 1
+            y += 1
+        return walls, path, start, end
+
+    def assertPathEquals(self, walls, expected_path, path):
+        try:
+            self.assertEqual(sorted(path), sorted(expected_path))
+        except AssertionError:
+            print("expected")
+            self.draw(self.to_sketch(walls, expected_path))
+            print("given")
+            self.draw(self.to_sketch(walls, path))
+            self.fail()
+
     def test_maze(self):
         a = pf.AStar()
-        walls = ((0, 5), (1, 0), (1, 1), (1, 5), (2, 3),
-                 (3, 1), (3, 2), (3, 5), (4, 1), (4, 4), (5, 1))
-        a.init_grid(6, 6, walls, (0, 0), (5, 5))
-        path = a.solve()
-        self.assertEqual(path, [(0, 0), (0, 1), (0, 2), (1, 2), (1, 3), (1, 4),
-                                (2, 4), (3, 4), (3, 3), (4, 3), (5, 3), (5, 4),
-                                (5, 5)])
+        walls, expected_path, start, end = self.from_sketch([
+            "[#    ",
+            ".# ###",
+            ".. #  ",
+            " .#...",
+            " ...#.",
+            "## # ]",
+        ])
+        a.init_grid(6, 6, walls, start, end)
+        self.assertPathEquals(walls, expected_path, a.solve())
+
+    def test_maze_diagonal(self):
+        a = pf.AStar()
+        walls, expected_path, start, end = self.from_sketch([
+            "[#    ",
+            ".# ###",
+            ". #...",
+            " .#.#.",
+            " ...#.",
+            "## # ]",
+        ])
+        a.init_grid(6, 6, walls, start, end)
+        self.assertPathEquals(walls, expected_path, a.solve(True))
 
     def test_maze_no_walls(self):
         a = pf.AStar()
-        walls = ()
-        a.init_grid(6, 6, walls, (0, 0), (5, 5))
-        path = a.solve()
-        self.assertEqual(len(path), 11)
+        walls, expected_path, start, end = self.from_sketch([
+            "[...  ",
+            "   .  ",
+            "   .. ",
+            "    . ",
+            "    . ",
+            "    .]",
+        ])
+        a.init_grid(6, 6, walls, start, end)
+        self.assertPathEquals(walls, expected_path, a.solve())
+
+    def test_maze_no_walls_diagonal(self):
+        a = pf.AStar()
+        walls, expected_path, start, end = self.from_sketch([
+            "[     ",
+            " .    ",
+            "  .   ",
+            "   .  ",
+            "    . ",
+            "     ]",
+        ])
+        a.init_grid(6, 6, walls, start, end)
+        self.assertPathEquals(walls, expected_path, a.solve(True))
+
+    def test_start_eq_end(self):
+        a = pf.AStar()
+        walls, expected_path, start, end = self.from_sketch([
+            ".     ",
+            "      ",
+            "      ",
+            "      ",
+            "      ",
+            "      ",
+        ])
+        a.init_grid(6, 6, walls, (0,0), (0,0))
+        self.assertPathEquals(walls, expected_path, a.solve())
 
     def test_maze_no_solution(self):
         a = pf.AStar()
-        walls = ((0, 5), (1, 0), (1, 1), (1, 2), (1, 3), (1, 4), (1, 5),
-                 (2, 3), (3, 1), (3, 2), (3, 5), (4, 1), (4, 4), (5, 1))
-        a.init_grid(6, 6, walls, (0, 0), (5, 5))
-        self.assertIsNone(a.solve())
+        walls, expected_path, start, end = self.from_sketch([
+            "(#    ",
+            " # ###",
+            " # #  ",
+            " ##   ",
+            "    ##",
+            "##  #)",
+        ])
+        a.init_grid(6, 6, walls, start, end)
+        self.assertPathEquals(walls, expected_path, a.solve())
+
+    def test_maze_closest_solution(self):
+        a = pf.AStar()
+        walls, expected_path, start, end = self.from_sketch([
+            "[#    ",
+            ".# ###",
+            ".# #  ",
+            ".##   ",
+            "... ##",
+            "## .#)",
+        ])
+        a.init_grid(6, 6, walls, start, end)
+        self.assertPathEquals(walls, expected_path, a.solve(True, True))
+
+    def test_maze_closest_solution2(self):
+        a = pf.AStar()
+        walls, expected_path, start, end = self.from_sketch([
+            "[#    ",
+            ".# ###",
+            " # #  ",
+            " #)# #",
+            " # ## ",
+            "##### ",
+        ])
+        a.init_grid(6, 6, walls, start, end)
+        self.assertPathEquals(walls, expected_path, a.solve(True, True))
+
+    def test_maze_closest_solution3(self):
+        a = pf.AStar()
+        walls, expected_path, start, end = self.from_sketch([
+            "[#    ",
+            ".# ###",
+            ".# #  ",
+            ".##   ",
+            "....##",
+            "## ###",
+        ])
+        a.init_grid(6, 6, walls, start, (5, 5))
+        self.assertPathEquals(walls, expected_path, a.solve(True, True))
 
 if __name__ == '__main__':
     unittest.main()
